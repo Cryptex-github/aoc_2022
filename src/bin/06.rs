45a8c8357879dd6c5b699c2e70db4767782fa8e4
Cryptex <64497526+Cryptex-github@users.noreply.github.com>
2022-12-05 22:43:10 -0800

    Day 6

---
+++ src/bin/06.rs
@@ -0,0 +1,55 @@
+use std::collections::{HashSet, VecDeque};
+
+fn get_unique(input: &str, n: usize) -> Option<u32> {
+    let mut queue = VecDeque::with_capacity(n);
+
+    for (i, c) in (1..).zip(input.chars()) {
+        if i <= n {
+            queue.push_back(c)
+        } else {
+            queue.pop_front();
+            queue.push_back(c)
+        }
+
+        if queue.len() == n {
+            queue.make_contiguous();
+
+            if !(1..n).any(|i| queue.as_slices().0[i..].contains(&queue.get(i - 1).unwrap())) {
+                return Some(i as u32);
+            }
+        }
+    }
+
+    None
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
+    get_unique(input, 4)
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    get_unique(input, 14)
+}
+
+fn main() {
+    let input = &advent_of_code::read_file("inputs", 6);
+    advent_of_code::solve!(1, part_one, input);
+    advent_of_code::solve!(2, part_two, input);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let input = advent_of_code::read_file("examples", 6);
+        assert_eq!(part_one(&input), Some(5));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let input = advent_of_code::read_file("examples", 6);
+        assert_eq!(part_two(&input), Some(23));
+    }
+}
